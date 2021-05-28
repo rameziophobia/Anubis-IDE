ca6a859cacaa4f533612e6b51c6e4cbbe161b435
Ramez <rameznoshy@gmail.com>
2021-05-28 23:37:04 +0200

    test: add test_cases.py

---
+++ test_cases.py
@@ -0,0 +1,18 @@
+def print_hello_world():
+    print('Hello World!')
+
+def add(a, b):
+    result = a + b
+    print(result)
+    return result
+
+def add_default(a, b=7):
+    return a + b
+
+def mult_default(num1=17, num2=2):
+    return num1 * num2
+
+def print_param(a):
+    print(a)
+
+should_not_be_detected
